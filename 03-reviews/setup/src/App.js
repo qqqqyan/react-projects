2e5e573acc7fec923149d380d7a2efbae70fd84c
Liu Qianyan <liuqianyankk@qq.com>
2023-10-19 11:34:27 +0800

    feat: 03

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Review from './Review';
+import people from './data'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
 function App() {
-  return <h2>reviews project setup</h2>;
+
+  const [index, setIndex] = useState(0);
+
+  const next = () => {
+    if (index === people.length - 1) {
+      setIndex(0)
+    } else {
+      setIndex(index + 1)
+    }
+  }
+  const last = () => {
+    if (index === 0) {
+      setIndex(people.length - 1)
+    } else {
+      setIndex(index - 1)
+    }
+  }
+
+  return (
+    <main>
+      <div className='container'>
+        <h2 className='title'>Our Reviews</h2>
+
+        <article className='review'>
+          <Review index={index}></Review>
+          <button className='prev-btn' onClick={last}>
+            <FaChevronLeft />
+          </button>
+          <button className='next-btn' onClick={next}>
+            <FaChevronRight />
+          </button>
+        </article>
+      </div>
+    </main>
+
+
+  )
 }
 
 export default App;
--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -2,8 +2,20 @@ import React, { useState } from 'react';
 import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
-const Review = () => {
-  return <h2>review component</h2>;
+const Review = ({ index }) => {
+  const peopleShow = people[index]
+
+  const { name, job, image, text } = peopleShow;
+  return (
+    <>
+      <div className='img-container'>
+        <img className='person-img' src={image} />
+      </div>
+      <div>{name}</div>
+      <div>{job}</div>
+      <div className='info'>{text}</div>
+    </>
+  )
 };
 
 export default Review;
