206e356dd8774090a09c50ca3220b179582c67db
liuqianyan <liuqianyan@izkun.com>
2023-10-21 23:19:22 +0800

    feat: 10-question

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,7 +3,63 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [grocery, setGrocery] = useState('')
+  const [id, setId] = useState(0)
+  const [groceryList, setGroceryList] = useState([])
+
+  const [index, setIndex] = useState(0)
+  const [editing, setEditing] = useState(false)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!editing) {
+      setId(id + 1) // 下面的取值会加一吗？多个set在一起会怎么处理？
+      setGroceryList([...groceryList, {
+        id,
+        name: grocery,
+      }])
+    } else {
+      groceryList[index].name = grocery //需要复制数组吗？最好要
+      setGroceryList(groceryList)
+      setEditing(false)
+    }
+    setGrocery('')
+  }
+  const handleEdit = (idx) => {
+    setGrocery(groceryList[idx].name)
+    setIndex(idx)
+    setEditing(true)
+  }
+  const handleDelete = (idx) => {
+    const newList = groceryList.filter((item, index) => index !== idx)
+    setGroceryList(newList)
+  }
+  const handleClear = () => {
+    setGroceryList([])
+    setId(0) //需要重设id吗？
+  }
+  return (
+    <section className='section-center'>
+      <Alert />
+
+      <h2>grocery bud</h2>
+      <form onSubmit={handleSubmit} className='form-control'>
+        <input type="text" className='grocery' value={grocery} onChange={(e) => setGrocery(e.target.value)} />
+        <button type="submit" className='submit-btn'>
+          {editing ? 'Edit' : 'Add'}
+        </button>
+      </form>
+      {
+        groceryList.length > 0 &&
+        <List
+          groceryList={groceryList}
+          onEdit={handleEdit}
+          onDelete={handleDelete}
+          onClear={handleClear}
+        />
+      }
+    </section>
+
+  )
 }
 
 export default App
--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,7 +1,29 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
+const List = ({ groceryList, onEdit, onDelete, onClear }) => {
+  return (
+    <div className='grocery-container'>
+      {
+        groceryList.map((grocery, index) => {
+          return (
+            <div key={grocery.id} className='grocery-item'>
+              <div className='title'>{grocery.name}</div>
+              <div className='btn-container'>
+                <button className='edit-btn' onClick={() => onEdit(index)}>
+                  <FaEdit />
+                </button>
+                <button className='delete-btn' onClick={() => onDelete(index)}>
+                  <FaTrash />
+                </button>
+              </div>
+            </div>
+          )
+        })
+      }
+      <button className='clear-btn' onClick={() => onClear()}>Clear Item</button>
+    </div>
+
+  )
 }
 
 export default List
